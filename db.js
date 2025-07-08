3b72f014ecde60e1c713fcb61bdbf12fe9edd4f8
viniciusP23 <vinicius_pereira1@live.com>
2025-07-07 22:49:04 -0300

    Login-API-mongoDB

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://testemongo01:Palmeiras1@cluster0.fagscgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("✅ Conectado ao MongoDB Atlas"))
 .catch((err) => console.error("❌ Erro ao conectar:", err))
